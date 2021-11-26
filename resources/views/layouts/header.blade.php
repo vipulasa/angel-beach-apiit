43ebd21f9bc03520262d0e45e416e0a76d325246
Vipula S Anandapiya <vipula@adlux.asia>
2021-11-26 11:33:32 +0530

    Admin gate added with the validation to check if the user has the is_admin value true

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,14 +3,38 @@
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'Laravel') }}
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
+                @if(auth()->check() && auth()->user()->is_admin)
+                    {{--this works but wrong when it comes to large applications and templates--}}
+                @endif
 
+                @can('access-administration')
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            Administration
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('admin.users.index') }}">
+                                <i class="fas fa-user-cog"></i>
+                                <span>Users</span>
+                            </a>
+                            <a class="dropdown-item" href="{{ route('admin.roles.index') }}">
+                                <i class="fas fa-user-cog"></i>
+                                <span>Roles</span>
+                            </a>
+                        </div>
+                    </li>
+                @endcan
             </ul>
 
             <!-- Right Side Of Navbar -->
@@ -30,7 +54,8 @@
                     @endif
                 @else
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
                         </a>
 
